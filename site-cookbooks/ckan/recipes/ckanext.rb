35d413b8bfe1cb4e3b779eee40e11d55118a9340
Braulio Chavez <pataslargas27@gmail.com>
2013-12-02 15:52:31 -0800

    Activates the ckanext-custom_theme plugin

--- site-cookbooks/ckan/recipes/ckanext.rb
+++ site-cookbooks/ckan/recipes/ckanext.rb
@@ -3,6 +3,7 @@ HOME = "/home/#{USER}"
 ENV['VIRTUAL_ENV'] = "#{HOME}/pyenv"
 ENV['PATH'] = "#{ENV['VIRTUAL_ENV']}/bin:#{ENV['PATH']}"
 SOURCE_DIR = "#{HOME}/ckan"
+CKAN_PYENV_SRC_DIR = "#{ENV['VIRTUAL_ENV']}/src"
 
 # Create Database
 pg_user "ckanuser" do
@@ -45,4 +46,35 @@ end
 execute "set permissions" do
   cwd SOURCE_DIR
   command "paster datastore set-permissions postgres"
+end
+
+# Set the custom theme plugin
+
+execute "delete the previous ckanext-custom_theme folder" do
+  user USER
+  cwd CKAN_PYENV_SRC_DIR
+
+  command "rm -fr ckanext-custom_theme/"
+  action :run
+end
+
+execute "clone ckanext-custom_theme files" do
+  user USER
+  cwd CKAN_PYENV_SRC_DIR
+
+  command "git clone https://github.com/CodeandoMexico/ckanext-custom_theme.git"
+  action :run
+end
+
+execute "run python setup.py develop to install the ckanext-custom_theme dir" do
+  user USER
+  cwd "#{CKAN_PYENV_SRC_DIR}/ckanext-custom_theme"
+
+  command "python setup.py develop"
+end
+
+execute "add the ckanext-custom_theme plugin to the settings" do
+  user USER
+  cwd SOURCE_DIR
+  command "sed -i -e 's/.*ckan\\.plugins.*/& custom_theme/' #{node[:environment]}.ini"
 end
