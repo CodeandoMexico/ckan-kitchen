496f1f06178010c03039541627396df82c110d5c
Braulio Chavez <braulio.vladimir.cn@gmail.com>
2014-02-18 15:19:09 -0600

    Activates the datapusher

--- site-cookbooks/ckan/recipes/ckanext.rb
+++ site-cookbooks/ckan/recipes/ckanext.rb
@@ -5,6 +5,8 @@ ENV['PATH'] = "#{ENV['VIRTUAL_ENV']}/bin:#{ENV['PATH']}"
 SOURCE_DIR = "#{HOME}/ckan"
 CKAN_PYENV_SRC_DIR = "#{ENV['VIRTUAL_ENV']}/src"
 
+# Activate Datastore
+
 # Create Database
 pg_user "ckanuser" do
   privileges :superuser => true, :createdb => true, :login => true
@@ -46,6 +48,161 @@ end
 execute "set permissions" do
   cwd SOURCE_DIR
   command "paster datastore set-permissions postgres -c #{node[:environment]}.ini"
+end
+
+# Activate Datapusher
+
+# install requirements for the datapusher
+apt_package "python-dev" do
+  action :install
+end
+
+apt_package "python-virtualenv" do
+  action :install
+end
+
+apt_package "build-essential" do
+  action :install
+end
+
+apt_package "libxslt1-dev" do
+  action :install
+end
+
+apt_package "libxml2-dev" do
+  action :install
+end
+
+# create a virtual env for datapusher
+python_virtualenv "#{HOME}/datapusher" do
+  owner USER
+  action :create
+end
+
+# create a source directory
+directory "#{HOME}/datapusher/src" do
+  owner "root"
+  mode 0777
+  action :create
+end
+
+# delete previous datapusher source folder if existent
+execute "delete previous datapusher source folder" do
+  cwd "#{HOME}/datapusher/src"
+
+  command "sudo rm -rf datapusher/"
+  action :run
+end
+
+# clone the source (always target the stable branch)
+execute "clone datapusher files" do
+  user USER
+  cwd "#{HOME}/datapusher/src"
+
+  command "git clone -b stable https://github.com/ckan/datapusher.git"
+  action :run
+end
+
+# install the Datapusher and its requirements
+python_pip "#{HOME}/datapusher/src/datapusher/requirements.txt" do
+  options "-r"
+  action :install
+end
+
+execute "run python setup.py develop to install datapusher" do
+  cwd "#{HOME}/datapusher/src/datapusher"
+
+  command "python setup.py develop"
+  action :run
+end
+
+# copy the standard Apache config file
+execute "copy the apache config file" do
+  cwd "#{HOME}/datapusher/src/datapusher"
+
+  command "sudo cp deployment/datapusher /etc/apache2/sites-available/"
+  action :run
+end
+
+# edit apache config file
+execute "edit datapusher apache config file" do
+  cwd "/etc/apache2/sites-available/"
+
+  command "sudo sed -i -e 's/\\/etc\\/ckan\\//\\/home\\/ckan\\/datapusher\\//' datapusher"
+  action :run
+end
+
+# copy the standard Datapusher wsgi file
+execute "copy the datapusher wsgi file" do
+  user USER
+  cwd "#{HOME}/datapusher/src/datapusher"
+
+  command "cp deployment/datapusher.wsgi #{HOME}/datapusher"
+  action :run
+end
+
+# edit datapusher wsgi file
+execute "edit datapusher wsgi file" do
+  user USER
+  cwd "#{HOME}/datapusher"
+
+  command "sed -i -e 's/\\/usr\\/lib\\/ckan\\//\\/home\\/ckan\\//;s/\\/etc\\/ckan/\\/home\\/ckan\\/datapusher/' datapusher.wsgi"
+  action :run
+end
+
+# copy the standard Datapusher settings
+execute "copy the standard datapusher settings" do
+  user USER
+  cwd "#{HOME}/datapusher/src/datapusher"
+
+  command "cp deployment/datapusher_settings.py #{HOME}/datapusher"
+  action :run
+end
+
+# open up port 8800 on Apache where the Datapusher accepts connections
+# make sure you only run these 2 functions once otherwise you will need
+# to manually edit /etc/apache2/ports.conf
+execute "open up port 8800" do
+  cwd HOME
+
+  command "sudo sh -c 'echo \"NameVirtualHost *:8800\" >> /etc/apache2/ports.conf'"
+  action :run
+end
+
+execute "listen up port 8800" do
+  cwd HOME
+
+  command "sudo sh -c 'echo \"Listen 8800\" >> /etc/apache2/ports.conf'"
+  action :run
+end
+
+# enable datapusher apache site
+execute "enable datapusher apache site" do
+  command "sudo a2ensite datapusher"
+  action :run
+end
+
+# configure ckan to use datapusher
+execute "set datapusher url and site_url" do
+  user USER
+  cwd SOURCE_DIR
+
+  command "sed -i -e 's/.*datapusher.url.*/ckan.datapusher.url=http:\\/\\/0.0.0.0:8800\\//;s/.*site_url.*/ckan.site_url=http:\\/\\/#{node[:ckan][:server_name]}/' #{node[:environment]}.ini"
+  action :run
+end
+
+# activate datapusher plugin
+execute "activate datapusher plugin in config file" do
+  user USER
+  cwd SOURCE_DIR
+  command "sed -i -e 's/.*ckan\\.plugins.*/& datapusher/' #{node[:environment]}.ini"
+  action :run
+end
+
+# restart the apache service
+execute "restart the apache service" do
+  command "sudo service apache2 restart"
+  action :run
 end
 
 # Set the custom theme plugin
