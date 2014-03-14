356af8aaa986e0bb6a95aa01e3160c2d606a0a15
Miguel Martinez <miguelmtz93@gmail.com>
2014-03-14 16:23:34 -0600

    data API's lightbox now uses datamx.io

--- site-cookbooks/ckan/attributes/default.rb
+++ site-cookbooks/ckan/attributes/default.rb
@@ -6,6 +6,6 @@ when "staging"
   default["ckan"]["server_name"] = "api.codeandomexico.org"
   default["ckan"]["server_alias"] = "api.codeandomexico.org"
 when "production"
-  default["ckan"]["server_name"] = "datos.codeandomexico.org"
-  default["ckan"]["server_alias"] = "datos.codeandomexico.org"
+  default["ckan"]["server_name"] = "datamx.io"
+  default["ckan"]["server_alias"] = "datamx.io"
 end
